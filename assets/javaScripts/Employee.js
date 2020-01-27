26ee1496a792c9cb37e7ca191729f9e207fd1fe7
Keylee <lineberry.kelly@gmail.com>
2020-01-27 14:46:09 -0700

    created class & constructor for Employee

--- assets/javaScripts/Employee.js
+++ assets/javaScripts/Employee.js
@@ -4,3 +4,17 @@ const path = require("path");
 const fs = require("fs");
 
 //Employee Constructor Function
+class Employee {
+    constructor (name, id, email, title) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.title = title;
+    }
+};
+
+
+console.log(Employee);
+
+//Exports the Employee Constructor & classes
+module.exports = Employee;
